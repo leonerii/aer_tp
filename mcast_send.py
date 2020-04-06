72368e3fe2baa5c0c7c6138f069185893fd5bc9a
Igor Virgilio <igor.virgilio@gmail.com>
2020-04-06 20:32:05 +0100

    start version

--- mcast_send.py
+++ mcast_send.py
@@ -1,16 +1,23 @@
 import socket
+import time
 
 MCAST_GRP = 'FF02::1'
 MCAST_PORT = 9999
-# regarding socket.IP_MULTICAST_TTL
-# ---------------------------------
-# for all packets sent, after two hops on the network the packet will not 
-# be re-sent/broadcast (see https://www.tldp.org/HOWTO/Multicast-HOWTO-6.html)
 MULTICAST_TTL = 2
+hello_time = 2
 
-sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)#, socket.IPPROTO_UDP)
-sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_LOOP, True)#socket.IPV6_MULTICAST_TTL, MULTICAST_TTL)
+def main():
+    send()
 
-# For Python 3, change next line to 'sock.sendto(b"robot", ...' to avoid the
-# "bytes-like object is required" msg (https://stackoverflow.com/a/42612820)
-sock.sendto(b"robot", (MCAST_GRP, MCAST_PORT))
+def send():
+    sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)#, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_LOOP, True)#socket.IPV6_MULTICAST_TTL, MULTICAST_TTL)
+
+    
+    while True:
+        time.sleep(hello_time)
+        sock.sendto(b"robot", (MCAST_GRP, MCAST_PORT))
+        print ("mensagem enviada\n")
+
+if __name__ == '__main__':
+    main()
