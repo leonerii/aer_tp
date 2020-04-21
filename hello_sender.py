4212521c89ce689179cb5e159f11f9f14c5d6fcc
Adriano Seie <aseie@Adrianos-MBP.lan>
2020-04-21 20:20:05 +0100

    create client socket

--- hello_sender.py
+++ hello_sender.py
@@ -21,23 +21,13 @@ class SendMessage(Thread):
             self.lock.release()
     
     def create_socket(self):
-        # hops
-        self.mcast_ttl = 2
-        self.mcast_group = 'FF02::1'
-        self.ttl = struct.pack('@i', mcast_ttl)
-        
-        # De acordo com o tipo de IP (ipv4 ou ipv6) definir a familia
-        addrinfo = socket.getaddrinfo(mcast_group_ipv4, None)[0]
-        
         try:
             # Criar o socket do client
-            self.client_sock = socket.socket(addrinfo[0], socket.SOCK_DGRAM)
+            self.client_sock = socket.socket(self.addrinfo[0], socket.SOCK_DGRAM)
             self.client.socket.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_HOPS, self.ttl)
          
         except Exception as sock_error:
             print(f'Failed to create socket: {sock_error}')
-
-        return udp_socket
 
     def hello_sender(self, route_table):
 
@@ -60,13 +50,13 @@ class SendMessage(Thread):
                         self.msg[keys] = values['timestamp']
 
                 print(f'Sending multicast message to the multicast group ...')
-                self.create_socket().sendto(str(self.msg).encode('utf-8'), (self.mcast_group,self.mcast_port))
+                self.client_socket.sendto(str(self.msg).encode('utf-8'), (self.mcast_group,self.mcast_port))
 
             except socket.gaierror as socket_error:
                 print(f'Sending error \'gaierror\': {socket_error}')
                 break
             finally:
-                self.create_socket().close()
+                self.client_socket.close()
         
         sleep(self.hello_interval)
 
