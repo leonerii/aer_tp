40257edb47a945ea51125cc524c8a4f86636371d
Adriano Seie <aseie@Adrianos-MacBook-Pro.local>
2020-04-21 18:28:34 +0100

    change hello message to include neighbors

--- hello_sender.py
+++ hello_sender.py
@@ -1,15 +1,13 @@
 from threading import Thread, RLock
 from time import sleep
+import struct
 
 class SendMessage(Thread):
-    def __init__(self, route_table :dict, lock :RLock, mcast_group, mcast_port, sock):
+    def __init__(self, route_table :dict, lock :RLock):
 
         Thread.__init__(self)
         self.route_table    = route_table
         self.lock           = lock
-        self.mcast_group = mcast_group
-        self.mcast_port = mcast_port
-        self.sock   = sock
             
     def run(self):
         try:
@@ -21,6 +19,25 @@ class SendMessage(Thread):
 
         finally:
             self.lock.release()
+    
+    def create_socket(self):
+        # hops
+        self.mcast_ttl = 2
+        self.mcast_group = 'FF02::1'
+        self.ttl = struct.pack('@i', mcast_ttl)
+        
+        # De acordo com o tipo de IP (ipv4 ou ipv6) definir a familia
+        addrinfo = socket.getaddrinfo(mcast_group_ipv4, None)[0]
+        
+        try:
+            # Criar o socket do client
+            self.client_sock = socket.socket(addrinfo[0], socket.SOCK_DGRAM)
+            self.client.socket.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_HOPS, self.ttl)
+         
+        except Exception as sock_error:
+            print(f'Failed to create socket: {sock_error}')
+
+        return udp_socket
 
     def hello_sender(self, route_table):
 
@@ -30,21 +47,26 @@ class SendMessage(Thread):
         '''
         self.hello_interval = 30
 
-        self.msg = {
-                "type": "HELLO"
-                }
-        self.msg.update(self.route_table)
-
         while True:
             try:
-                print(f'Sending multicast message to the multicast group: {self.mcast_group} ...')
-                self.sock.sendto(str(self.msg).encode('utf-8'), (self.mcast_group,self.mcast_port))
+                # Messagem a ser enviada
+
+                self.msg = {
+                "type": "HELLO"
+                }
+
+                for keys, values in list(route_table.items()):
+                    if values['next_hop'] == None:
+                    self.msg[keys] = values['timestamp']
+
+                print(f'Sending multicast message to the multicast group ...')
+                self.create_socket().sendto(str(self.msg).encode('utf-8'), (self.mcast_group,self.mcast_port))
 
             except socket.gaierror as socket_error:
                 print(f'Sending error \'gaierror\': {socket_error}')
                 break
             finally:
-                self.sock.close()
+                self.create_socket().close()
         
         sleep(self.hello_interval)
 
