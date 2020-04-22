256bbfdb1e2b170a89ea468def32845c305f5bea
Leonardo Neri <leonerii>
2020-04-22 16:14:49 +0100

    hotfix

--- hello_sender.py
+++ hello_sender.py
@@ -3,62 +3,67 @@ from time import sleep
 import os
 import uuid
 from signal import SIGUSR1
+import struct
+import socket
 
 class SendMessage(Thread):
-    def __init__(self, route_table :dict, lock :RLock, mcast_group, mcast_port,  hello_interval):
+    def __init__(self, route_table :dict, lock :RLock, hello_interval, ttl, mcast_group, mcast_port):
 
         Thread.__init__(self)
         self.route_table    = route_table
         self.lock           = lock
+        self.hello_interval = hello_interval
+        self.ttl            = ttl
+        self.mcast_group    = mcast_group
+        self.mcast_port     = mcast_port
+        self.create_socket()
             
     def run(self):
-        try:
-            self.lock.acquire()
-            self.hello_sender()
+        while True: 
+            try:
+                self.lock.acquire()
+                self.hello_sender()
 
-        except Exception as e:
-            print(f'Failed: {e}')
+            except Exception as e:
+                print(f'Failed: {e}')
 
-        finally:
-            self.lock.release()
+            finally:
+                self.lock.release()
+
+        sleep(self.hello_interval)
     
     def create_socket(self):
 
         try:
             # Criar o socket do client
-            self.client_sock = socket.socket(addrinfo[0], socket.SOCK_DGRAM)
-            self.client.socket.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_HOPS, self.ttl)
+            self.client_sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+            self.client_sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_HOPS, self.ttl)
          
         except Exception as sock_error:
             print(f'Failed to create socket: {sock_error}')
 
-    def hello_sender(self, route_table):
+    def hello_sender(self):
 
         '''
         Envia Messagem do tipo "HELLO" juntamente com a tabela de roteamento (route_table)
         e fecha o socket
         '''
-       
-        while True:
-            try:
-                # Messagem a ser enviada
-
-                self.msg = {
+        try:
+            # Messagem a ser enviada
+            self.msg = {
                 "type": "HELLO"
-                }
+            }
 
-                for keys, values in list(route_table.items()):
-                    if values['next_hop'] == None:
-                        self.msg[keys] = values['timestamp']
+            for keys, values in self.route_table.items():
+                if values['next_hop'] == None:
+                    self.msg[keys] = values['timestamp']
 
-                print(f'Sending multicast message to the multicast group ...')
-                self.client_socket.sendto(str(self.msg).encode('utf-8'), (self.mcast_group,self.mcast_port))
+            print(f'Sending multicast message to the multicast group ...')
+            self.client_socket.sendto(str(self.msg).encode('utf-8'), (self.mcast_group,self.mcast_port))
 
-            except socket.gaierror as socket_error:
-                print(f'Sending error \'gaierror\': {socket_error}')
-                break
-            finally:
-                self.client_socket.close()
-        
-        sleep(self.hello_interval)
-
+        except socket.gaierror as socket_error:
+            print(f'Sending error \'gaierror\': {socket_error}')
+            break
+        finally:
+            self.client_socket.close()
+    
