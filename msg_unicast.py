28f8c95a4270d98619f5019117771de720bf53c6
igorvirgilio <igor.virgilio@gmail.com>
2020-04-22 16:40:52 +0100

    add msg_unicast

---
+++ msg_unicast.py
@@ -0,0 +1,12 @@
+import socket
+
+
+
+def create_socket():
+    sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    return sock
+def send_unicast(msg, uni_addr, mcast_port):
+    sock = create_socket()
+    sock.sendto(str(msg).encode('utf-8'), (uni_addr, mcast_port))
